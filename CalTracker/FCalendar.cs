de235ddd248958b9d348c311940bd6878aea700d
finnegantdewitt <finn@finnegandewitt.com>
2020-06-01 10:40:09 -0700

    Add project files.

---
+++ CalTracker/FCalendar.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace CalTracker
+{
+    class FCalendar 
+    {
+        readonly CultureInfo myCI = new CultureInfo("en-US");
+        public Calendar calendar;
+        readonly DateTime now = DateTime.Now;
+        public List<FDay> days;
+
+        public FCalendar()
+        {
+            calendar = myCI.Calendar;
+            days = LoadFDays(now.Year);
+        }
+
+        private List<FDay> LoadFDays(int year)
+        {
+            List<FDay> fDays = new List<FDay>();
+            int week = 0;
+            for (int month = 1; month <= calendar.GetMonthsInYear(year); month++)
+            {
+                for(int day = 1; day <= calendar.GetDaysInMonth(year, month); day++)
+                {
+                    FDay fDay = new FDay(year, month, day, week);
+                    Console.Write(fDay.ToString() + ", ");
+                    if (fDay.DayOfWeek == 6)
+                    {
+                        week++;
+                        Console.WriteLine();
+                    }
+                    fDays.Add(fDay);
+                }
+            }
+            return fDays;
+        }
+        public List<FDay> MonthView(int month)
+        {
+            List<FDay> monthList = new List<FDay>();
+            (int, int) monthColumnRange = MonthColumnRange(month);
+            foreach(FDay day in days)
+            {
+                if ((day.WeekOfYear >= monthColumnRange.Item1) && (day.WeekOfYear <= monthColumnRange.Item2))
+                {
+                    day.RowInCalView = day.WeekOfYear - monthColumnRange.Item1;
+                    monthList.Add(day);
+                }
+                    
+            }
+            return monthList;
+        }
+        //TODO:
+        //add error for out of range for calendar
+        //scollable by row/(week)
+        private (int, int) MonthColumnRange(int month)
+        {
+            foreach(FDay day in days)
+            {
+                if(day.Month == month)
+                {
+                    return (day.WeekOfYear, day.WeekOfYear + 5);
+                }
+            }
+            throw new Exception("Something happened in the MonthColumnRange");
+        }
+    }
+}
---
+++ CalTracker/FDay.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalTracker
+{
+    class FDay 
+    {
+        DateTime dateTime;
+        bool highlighted = false;
+        public int Column { get; set; }
+        public int WeekOfYear { get; set; }
+        public int RowInCalView { get; set; }
+        
+        public FDay(int year, int month, int day)
+        {
+            dateTime = new DateTime(year, month, day);
+        }
+        public FDay(int year, int month, int day, int week)
+        {
+            dateTime = new DateTime(year, month, day);
+            WeekOfYear = week;
+            Column = this.DayOfWeek;
+        }
+        public string Day
+        {
+            get
+            {
+                return dateTime.Day.ToString();
+            }
+        }
+        public int Month
+        {
+            get
+            {
+                return dateTime.Month;
+            }
+        }
+        public int DayOfWeek
+        {
+            get
+            {
+                return (int)dateTime.DayOfWeek;
+            }
+        }
+        public override string ToString()
+        {
+            return " " + Column + " " + WeekOfYear;
+        }
+    }
+}
